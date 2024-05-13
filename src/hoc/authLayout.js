9b8a7199c1b8c0925c7b47c7c708612fd78aed2c
Your Name <you@example.com>
2024-05-13 07:47:24 -0400

    add a image for login page

--- src/hoc/authLayout.js
+++ src/hoc/authLayout.js
@@ -13,7 +13,7 @@ const authLayout = (ChildComponent) => {
                 <div className="container-fluid h-custom">
                     <div className="row d-flex justify-content-center align-items-center h-100">
                         <div className="col-md-9 col-lg-6 col-xl-5">
-                            <img alt="hey" src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp" className="img-fluid"/>
+                            <img alt="hey" src={require('./../assets/images/login_img.png')} className="img-fluid"/>
                         </div>
                         <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
                             <ChildComponent {...this.props} />
