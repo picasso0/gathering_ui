0f4bebc23b2ee788ef956a76dff75ab051c9f24a
Your Name <you@example.com>
2024-05-15 03:58:06 -0400

    fix statuses in worker userdata and profiles

--- src/pages/Profiles.js
+++ src/pages/Profiles.js
@@ -5,139 +5,146 @@ import axios from 'axios';
 import ModalComponent from "../components/ModalComponent";
 
 class Profiles extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          data: [],
-          loading: true,
-          currentPage: 1,
-          totalPages: 1,
-          limit: 10,
-          profile: {}
-        };
-      }
-      
-      componentDidMount() {
-        this.fetchData();
-      }
-     async get_profile(mobile) {
-        const accessToken = localStorage.getItem('accessToken');
-        try {
-          const response = await axios.get(process.env.REACT_APP_API_URL + `/profile/?mobile=${mobile}`,{
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-            },
-          });
-          this.setState({
-            profile: response.data,
-            loading: false,
-          });
-        } catch (error) {
-          console.error('Error fetching data:', error);
-          window.location.href = '/login';
-          this.setState({
-            loading: false,
-          });
-        }
-      }
-      fetchData = async () => {
-        const accessToken = localStorage.getItem('accessToken');
-        try {
-          const response = await axios.get(process.env.REACT_APP_API_URL + `/profiles/?page=${this.state.currentPage}&limit=${this.state.limit}`,{
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-            },
-          });
-          this.setState({
-            data: response.data.data,
-            totalPages: Math.ceil(response.data.total_count / this.state.limit) ,
-            loading: false
-          });
-        } catch (error) {
-          console.error('Error fetching data:', error);
-          window.location.href = '/login';
-          this.setState({
-            loading: false
-          });
-        }
-      };
-    
-      goToPage = (page) => {
-        if (page >= 1 && page <= this.state.totalPages) {
-          this.setState({ currentPage: page }, this.fetchData);
-        }
-      };
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      loading: true,
+      currentPage: 1,
+      totalPages: 1,
+      limit: 10,
+      profile: {}
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+  async get_profile(mobile) {
+    const accessToken = localStorage.getItem('accessToken');
+    try {
+      const response = await axios.get(process.env.REACT_APP_API_URL + `/profile/?mobile=${mobile}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      this.setState({
+        profile: response.data,
+        loading: false,
+      });
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      window.location.href = '/login';
+      this.setState({
+        loading: false,
+      });
+    }
+  }
+  fetchData = async () => {
+    const accessToken = localStorage.getItem('accessToken');
+    try {
+      const response = await axios.get(process.env.REACT_APP_API_URL + `/profiles/?page=${this.state.currentPage}&limit=${this.state.limit}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      this.setState({
+        data: response.data.data,
+        totalPages: Math.ceil(response.data.total_count / this.state.limit),
+        loading: false
+      });
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      window.location.href = '/login';
+      this.setState({
+        loading: false
+      });
+    }
+  };
+
+  goToPage = (page) => {
+    if (page >= 1 && page <= this.state.totalPages) {
+      this.setState({ currentPage: page }, this.fetchData);
+    }
+  };
 
 
-    render(){
-        const { data, loading, currentPage, totalPages, profile } = this.state;
+  render() {
+    const { data, loading, currentPage, totalPages, profile } = this.state;
 
     if (loading) {
       return <div>Loading...</div>;
     }
-        return (
-          <div>
-            {console.log(this.state)}
-            <div className="d-flex text-muted">
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th>id</th>
-                                <th>mobile</th>
-                                <th>data</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {data.map((item) => (
-                            <>
-                            <tr>
-                                <td>
-                                {item._id}
-                            </td>
-                            <td>
-                            {item.mobile}
-                            </td>
-                            <td>
-                            <td>
-                            <button onClick={()=>this.get_profile(item.mobile)} type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#rightModalDefault">
-                                        show data
-                                    </button>
-                                    <ModalComponent title={profile.mobile} content={<><div style={{ border: '1px solid #ccc', padding: '10px', borderRadius: '5px', backgroundColor: '#f9f9f9' }}>
-              <pre>{JSON.stringify(profile, null, 2)}</pre>
-            </div></>} className="right" dataBsBackdrop="static" id="rightModalDefault"/>
+    return (
+      <div>
+        {console.log(this.state)}
+        <div className="d-flex text-muted">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>id</th>
+                <th>mobile</th>
+                <th>data</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((item) => (
+                <>
+                  <tr>
+                    <td>
+                      {item._id}
+                    </td>
+                    <td>
+                      {item.mobile}
+                    </td>
+                    <td>
+                      {item.whatsapp_searching === 0 ? (
+                        <span className="bg-primary">ready</span>
+                      ) : (
+                        item.status === 1 ? (
+                          <span className="bg-success">in searching</span>
+                        ) : <span className="bg-danger">failed</span>
+                      )}
+                    </td>
+                    <td>
+                      <button onClick={() => this.get_profile(item.mobile)} type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#rightModalDefault">
+                        show data
+                      </button>
+                      <ModalComponent title={profile.mobile} content={<><div style={{ border: '1px solid #ccc', padding: '10px', borderRadius: '5px', backgroundColor: '#f9f9f9' }}>
+                        <pre>{JSON.stringify(profile, null, 2)}</pre>
+                      </div></>} className="right" dataBsBackdrop="static" id="rightModalDefault" />
 
-                            </td>
-                            </td>
-                            </tr>
-                        </>
+                    </td>
+                  </tr>
+                </>
               ))}
-                       </tbody>
-                    </table>
-                </div>
-                <nav className="table-bottom-center-pagination" aria-label="Page navigation example ">
-                    <ul className="pagination">
-                        <li className="page-item">
-                        <a onClick={() => this.goToPage(currentPage - 1)} className="page-link" href="#" aria-label="Previous">
-                            <span aria-hidden="true">&laquo;</span>
-                            <span className="sr-only">Previous</span>
-                        </a>
-                        </li>
-                        <li>
-                        <span  className="text-center text-small p-2 mt-2">Page {currentPage} of {totalPages}</span>
-                        </li>
-                        <li className="page-item">
-                        <a onClick={() => this.goToPage(currentPage + 1)} className="page-link" href="#" aria-label="Next">
-                            <span aria-hidden="true">&raquo;</span>
-                            <span className="sr-only">Next</span>
-                        </a>
-                        </li>
-                    </ul>
-                </nav>
-            <div>
-            </div>
-          </div>
-        );
-      }
+            </tbody>
+          </table>
+        </div>
+        <nav className="table-bottom-center-pagination" aria-label="Page navigation example ">
+          <ul className="pagination">
+            <li className="page-item">
+              <a onClick={() => this.goToPage(currentPage - 1)} className="page-link" href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+                <span className="sr-only">Previous</span>
+              </a>
+            </li>
+            <li>
+              <span className="text-center text-small p-2 mt-2">Page {currentPage} of {totalPages}</span>
+            </li>
+            <li className="page-item">
+              <a onClick={() => this.goToPage(currentPage + 1)} className="page-link" href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+                <span className="sr-only">Next</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default adminLayout(Profiles);
--- src/pages/Userdata.js
+++ src/pages/Userdata.js
@@ -5,117 +5,117 @@ import axios from 'axios';
 import ModalComponent from "../components/ModalComponent";
 
 class Userdata extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          data: [],
-          loading: true,
-          currentPage: 1,
-          totalPages: 1,
-          limit: 10,
-          userdata: {}
-        };
-      }
-      componentDidMount() {
-        this.fetchData();
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      loading: true,
+      currentPage: 1,
+      totalPages: 1,
+      limit: 10,
+      userdata: {}
+    };
+  }
+  componentDidMount() {
+    this.fetchData();
+  }
 
-      fetchData = async () => {
-        const accessToken = localStorage.getItem('accessToken');
-        try {
-          const response = await axios.get(process.env.REACT_APP_API_URL + `/userdatas/?page=${this.state.currentPage}&limit=${this.state.limit}`,{
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-            },
-          });
-          this.setState({
-            data: response.data.data,
-            totalPages: Math.ceil(response.data.total_count / this.state.limit) ,
-            loading: false
-          });
-        } catch (error) {
-          console.error('Error fetching data:', error);
-          window.location.href = '/login';
-          this.setState({
-            loading: false
-          });
-        }
-      };
-    
-      goToPage = (page) => {
-        if (page >= 1 && page <= this.state.totalPages) {
-          this.setState({ currentPage: page }, this.fetchData);
-        }
-      };
+  fetchData = async () => {
+    const accessToken = localStorage.getItem('accessToken');
+    try {
+      const response = await axios.get(process.env.REACT_APP_API_URL + `/userdatas/?page=${this.state.currentPage}&limit=${this.state.limit}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      this.setState({
+        data: response.data.data,
+        totalPages: Math.ceil(response.data.total_count / this.state.limit),
+        loading: false
+      });
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      window.location.href = '/login';
+      this.setState({
+        loading: false
+      });
+    }
+  };
+
+  goToPage = (page) => {
+    if (page >= 1 && page <= this.state.totalPages) {
+      this.setState({ currentPage: page }, this.fetchData);
+    }
+  };
 
 
-    render(){
-        const { data, loading, currentPage, totalPages, userdata } = this.state;
+  render() {
+    const { data, loading, currentPage, totalPages, userdata } = this.state;
 
     if (loading) {
       return <div>Loading...</div>;
     }
-        return (
-          <div>
-            {console.log(this.state)}
-            <div className="d-flex text-muted">
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th>id</th>
-                                <th>path</th>
-                                <th>status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {data.map((item) => (
-                            <>
-                            <tr>
-                                <td>
-                                {item._id}
-                            </td>
-                            <td>
-                            {item.path}
-                            </td>
-                            <td>
-                            {item.status==0 ? (
-        <>pending</>
-      ) : item.status==1 ? (
-        <>running</>
-      ) : (
-        <>failed</>
-      )}
-                            </td>
-                            </tr>
-                        </>
+    return (
+      <div>
+        {console.log(this.state)}
+        <div className="d-flex text-muted">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>id</th>
+                <th>path</th>
+                <th>status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((item) => (
+                <>
+                  <tr>
+                    <td>
+                      {item._id}
+                    </td>
+                    <td>
+                      {item.path}
+                    </td>
+                    <td>
+                      {item.status == 0 ? (
+                        <span className="bg-primary">ready</span>
+                      ) : item.status == 1 ? (
+                        <span className="bg-primary">in use</span>
+                      ) : (
+                        <span className="bg-danger">failed</span>
+                      )}
+                    </td>
+                  </tr>
+                </>
               ))}
-                       </tbody>
-                    </table>
-                </div>
-                <nav className="table-bottom-center-pagination" aria-label="Page navigation example ">
-                    <ul className="pagination">
-                        <li className="page-item">
-                        <a onClick={() => this.goToPage(currentPage - 1)} className="page-link" href="#" aria-label="Previous">
-                            <span aria-hidden="true">&laquo;</span>
-                            <span className="sr-only">Previous</span>
-                        </a>
-                        </li>
-                        <li>
-                        <span  className="text-center text-small p-2 mt-2">Page {currentPage} of {totalPages}</span>
-                        </li>
-                        <li className="page-item">
-                        <a onClick={() => this.goToPage(currentPage + 1)} className="page-link" href="#" aria-label="Next">
-                            <span aria-hidden="true">&raquo;</span>
-                            <span className="sr-only">Next</span>
-                        </a>
-                        </li>
-                    </ul>
-                </nav>
-            <div>
-            </div>
-          </div>
-        );
-      }
+            </tbody>
+          </table>
+        </div>
+        <nav className="table-bottom-center-pagination" aria-label="Page navigation example ">
+          <ul className="pagination">
+            <li className="page-item">
+              <a onClick={() => this.goToPage(currentPage - 1)} className="page-link" href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+                <span className="sr-only">Previous</span>
+              </a>
+            </li>
+            <li>
+              <span className="text-center text-small p-2 mt-2">Page {currentPage} of {totalPages}</span>
+            </li>
+            <li className="page-item">
+              <a onClick={() => this.goToPage(currentPage + 1)} className="page-link" href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+                <span className="sr-only">Next</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default adminLayout(Userdata);
